a452fb165618b558e7e0d4ed545b0f380a615e8b
Yana_Aziamblouskaya <yanaozembl@gmail.com>
2024-03-01 14:14:00 +0300

    commit

--- src/main/java/com/example/dataanalysermicroservice/config/BeanConfig.java
+++ src/main/java/com/example/dataanalysermicroservice/config/BeanConfig.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class BeanConfig {
+public class BeanConfig  {
 
     @Bean
     @SneakyThrows
